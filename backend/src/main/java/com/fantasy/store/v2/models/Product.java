38aa090234d5d98c0839128f31c52bc3d3b42bd2
Alistair <alistair13_cooke@hotmail.com>
2023-04-19 14:27:18 +0100

    initialised project

---
+++ backend/src/main/java/com/fantasy/store/v2/models/Product.java
@@ -0,0 +1,7 @@
+package com.fantasy.store.v2.models;
+
+public class Product {
+
+
+}
+
---
+++ backend/src/test/java/com/fantasy/store/v2/V2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fantasy.store.v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
