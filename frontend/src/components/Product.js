b768e5e019f327c8d2c36a096d404be93fcccd96
Alistair <alistair13_cooke@hotmail.com>
2023-05-19 18:35:32 +0100

    images of weapons render correctly based on the product name

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import styled from 'styled-components';
 import bronzeSwordImage from '../images/bronze_sword_pic.webp'
+import silverSwordImage from '../images/silver_sword_pic.jpeg'
+import goldSwordImage from '../images/gold_sword_pic.jpeg'
+import bronzeAxeImage from '../images/Bronze_axe_pic.webp'
+import silverAxeImage from '../images/silver_axe_pic.webp'
+import goldAxeImage from '../images/gold_axe_pic.webp'
+import bronzeSpearImage from '../images/bronze_spear_pic.webp'
+import silverSpearImage from '../images/silver_spear_pic.webp'
+import goldSpearImage from '../images/gold_spear_pic.png'
 
 
 function Product({product}) {
@@ -18,7 +26,15 @@ function Product({product}) {
             <div>
                 Markup : {Math.round((product.price - product.cost) / product.cost * 100)}%
             </div>
-            {product.productName === "Bronze Sword" ? <Img src={bronzeSwordImage} alt='Picture of Bronze Sword'/> : ""}
+            {product.productName.toLowerCase() === "bronze sword" ? <Img src={bronzeSwordImage} alt='Picture of Bronze Sword'/> : ""}
+            {product.productName.toLowerCase() === "silver sword" ? <Img src={silverSwordImage} alt='Picture Silver Sword'/> : ""}
+            {product.productName.toLowerCase() === "gold sword" ? <Img src={goldSwordImage} alt='Picture of Gold Sword'/> : ""}
+            {product.productName.toLowerCase() === "bronze axe" ? <Img src={bronzeAxeImage} alt='Picture of Bronze Axe'/> : ""}
+            {product.productName.toLowerCase() === "silver axe" ? <Img src={silverAxeImage} alt='Picture of Silver Axe'/> : ""}
+            {product.productName.toLowerCase() === "gold axe" ? <Img src={goldAxeImage} alt='Picture of Gold Axe'/> : ""}
+            {product.productName.toLowerCase() === "bronze spear" ? <Img src={bronzeSpearImage} alt='Picture of Bronze Spear'/> : ""}
+            {product.productName.toLowerCase() === "silver spear" ? <Img src={silverSpearImage} alt='Picture of Silver Spear'/> : ""}
+            {product.productName.toLowerCase() === "gold spear" ? <Img src={goldSpearImage} alt='Picture of Gold Spear'/> : ""}
         
         </BoxContainer>
     </Li>
