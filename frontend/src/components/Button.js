1527a7a224327c0489f81aabe5f2d1fb68ed59e2
Alistair <alistair13_cooke@hotmail.com>
2023-05-24 10:39:59 +0100

    added edit and delete buttons to single product page

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -9,10 +9,24 @@ const Button = ({children}) => {
 }
 
 const StyledButton = styled.button`
-    font-size: 1rem;
-    text-align: center;
-    
+  margin-top: 5%;
+  margin-bottom: 5%;
+  background-color: hsl(240, 40%, 65%);
+  border: none;
+  min-height: 40px;
+  min-width: 100px;
+  border-radius: 0.3em;
+  font-size: 1em;
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: bold;
+  font-style: italic;
+  color: white;
+  
+
+  cursor: pointer;
+  &:hover {
+    background-color: hsl(240, 40%, 70%);
+
+  }
 `
-
-
 export default Button;
--- frontend/src/containers/ListContainer.js
+++ frontend/src/containers/ListContainer.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 import Product from '../components/Product';
 import NewProduct from '../components/NewProduct';
+import Button from '../components/Button';
 
 function ListContainer({allProducts, view, setView, weaponsData, potionsData, armoursData}) {
     const [chosenProduct, setChosenProduct] = useState({});
@@ -56,6 +57,10 @@ function ListContainer({allProducts, view, setView, weaponsData, potionsData, ar
         {view === "single" ? 
             <SingleContainer>
                 {singleProduct}
+                <ButtonContainer>
+                    <Button type='button'>Edit</Button>
+                    <DeleteButton type='button'>Delete</DeleteButton>
+                </ButtonContainer>
             </SingleContainer> : ""}
 
         {view === "new" ? <SingleContainer><NewProduct /> </SingleContainer>: ""}
@@ -81,10 +86,42 @@ const ProductContainer = styled.ul`
 `
 
 const SingleContainer = styled.div`
+    margin-top: 1em;
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
     min-height: 50vh;
+   
+`
+const ButtonContainer = styled.div`
+    display: flex;
+    gap: 1em;
+
+`
+
+const DeleteButton = styled.button`
+    margin-top: 5%;
+    margin-bottom: 5%;
+    background-color: hsl(0, 60%, 50%);
+    border: none;
+    min-height: 40px;
+    min-width: 100px;
+    border-radius: 0.3em;
+    font-size: 1em;
+    font-family: Arial, Helvetica, sans-serif;
+    font-weight: bold;
+    font-style: italic;
+    color: white;
+  
+
+  cursor: pointer;
+  &:hover {
+    background-color: hsl(0, 60%, 60%);
+
+  }
+  
+
 `
 
 
